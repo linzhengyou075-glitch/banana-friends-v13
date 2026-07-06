0e6916d14f734a7697acb7ad08b549f73dedd379
linzhengyou075-glitch <linzhengyou075@gmail.com>
2026-07-07 03:59:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/webhook", require("./routes/webhook"));
+
 app.get("/", (req, res) => {
-  res.send("🍌 Banana Friends Ultimate V13 啟動成功！");
+  res.send("🍌 Banana Friends Ultimate V13");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on ${PORT}`);
+  console.log("Server Running：" + PORT);
 });
