e12a29211dc33a62f09eaf7887ae70820a7fd08d
linzhengyou075-glitch <linzhengyou075@gmail.com>
2026-07-07 09:06:39 +0800

    Update message.js

--- services/message.js
+++ services/message.js
@@ -1,39 +1,43 @@
 const reply = require("./reply");
 const { getUser } = require("./member");
+const { sign } = require("./sign");
+const { profileCard } = require("./flex");
 
 async function handle(event) {
 
   if (event.type !== "message") return;
-
   if (event.message.type !== "text") return;
 
   const text = event.message.text.trim();
+  const userId = event.source.userId;
 
-  const profile = await event.source.userId;
-
-  const user = await getUser(profile);
+  const user = await getUser(userId);
 
   switch (text) {
 
-    case "簽到":
-      return reply(event.replyToken, "🍌 簽到功能建置中");
+    case "簽到": {
+      const result = sign(user);
+      await user.save();
+      return reply(event.replyToken, result.message);
+    }
 
     case "我的資料":
-      return reply(event.replyToken, "👤 我的資料功能建置中");
+    case "等級":
+    case "名片":
+      return reply(event.replyToken, profileCard(user));
 
     case "排行榜":
-      return reply(event.replyToken, "🏆 排行榜功能建置中");
+      return reply(event.replyToken, "🏆 排行榜功能下一步加入");
 
     case "商店":
-      return reply(event.replyToken, "🛒 商店功能建置中");
+      return reply(event.replyToken, "🛒 商店功能下一步加入");
 
     case "抽獎":
-      return reply(event.replyToken, "🎰 抽獎功能建置中");
+      return reply(event.replyToken, "🎰 抽獎功能下一步加入");
 
     default:
       return;
   }
-
 }
 
 module.exports = handle;
