cb67e03e9452282d10f0a2f79448b133e701980f
linzhengyou075-glitch <linzhengyou075@gmail.com>
2026-07-07 10:07:37 +0800

    Update message.js

--- services/message.js
+++ services/message.js
@@ -8,7 +8,6 @@ const { sign } = require("./sign");
 const { profileCard } = require("./flex");
 
 async function handle(event) {
-
   if (event.type !== "message") return;
   if (event.message.type !== "text") return;
 
@@ -16,26 +15,22 @@ async function handle(event) {
   const userId = event.source.userId;
 
   const user = await getUser(userId);
+
   reward(user);
-await user.save();
+
+  await saveLog(user, "text", text, 2, 2);
+
+  await user.save();
+
   switch (text) {
     case "指令":
-case "幫助":
-case "教學":
-    return reply(event.replyToken, helpText());
+    case "幫助":
+    case "教學":
+      return reply(event.replyToken, helpText());
 
     case "簽到": {
-      reward(user);
-
-await user.save();
-
-await saveLog(
-    user,
-    "text",
-    text,
-    2,
-    2
-);
+      const result = sign(user);
+      await user.save();
       return reply(event.replyToken, result.message);
     }
 
@@ -45,42 +40,32 @@ await saveLog(
       return reply(event.replyToken, profileCard(user));
 
     case "排行榜":
-case "等級排行": {
-  const users = await levelTop(10);
+    case "等級排行": {
+      const users = await levelTop(10);
+      let msg = "🏆 等級排行榜 TOP10\n\n";
+      users.forEach((u, i) => {
+        msg += `${i + 1}. ${u.displayName || "蕉友"}｜Lv.${u.level}｜EXP ${u.exp}\n`;
+      });
+      return reply(event.replyToken, msg);
+    }
 
-  let msg = "🏆 等級排行榜 TOP10\n\n";
+    case "香蕉幣排行": {
+      const users = await bananaTop(10);
+      let msg = "🍌 香蕉幣排行榜 TOP10\n\n";
+      users.forEach((u, i) => {
+        msg += `${i + 1}. ${u.displayName || "蕉友"}｜${u.banana} 香蕉幣\n`;
+      });
+      return reply(event.replyToken, msg);
+    }
 
-  users.forEach((u, i) => {
-    msg += `${i + 1}. ${u.displayName || "蕉友"}｜Lv.${u.level}｜EXP ${u.exp}\n`;
-  });
-
-  return reply(event.replyToken, msg);
-}
-
-case "香蕉幣排行": {
-  const users = await bananaTop(10);
-
-  let msg = "🍌 香蕉幣排行榜 TOP10\n\n";
-
-  users.forEach((u, i) => {
-    msg += `${i + 1}. ${u.displayName || "蕉友"}｜${u.banana} 香蕉幣\n`;
-  });
-
-  return reply(event.replyToken, msg);
-}
-
-case "簽到排行": {
-  const users = await signTop(10);
-
-  let msg = "📅 簽到排行榜 TOP10\n\n";
-
-  users.forEach((u, i) => {
-    msg += `${i + 1}. ${u.displayName || "蕉友"}｜${u.signDays} 天\n`;
-  });
-
-  return reply(event.replyToken, msg);
-}
-    
+    case "簽到排行": {
+      const users = await signTop(10);
+      let msg = "📅 簽到排行榜 TOP10\n\n";
+      users.forEach((u, i) => {
+        msg += `${i + 1}. ${u.displayName || "蕉友"}｜${u.signDays} 天\n`;
+      });
+      return reply(event.replyToken, msg);
+    }
 
     case "商店":
       return reply(event.replyToken, "🛒 商店功能下一步加入");
