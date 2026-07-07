babebd35f495390a2e79683760bf4636b4eec83a
linzhengyou075-glitch <linzhengyou075@gmail.com>
2026-07-07 08:26:40 +0800

    Create economy.js

---
+++ services/services/economy.js
@@ -0,0 +1,18 @@
+function addBanana(user, amount) {
+  user.banana += amount;
+  return user;
+}
+
+function useBanana(user, amount) {
+  if (user.banana < amount) {
+    return false;
+  }
+
+  user.banana -= amount;
+  return true;
+}
+
+module.exports = {
+  addBanana,
+  useBanana
+};
